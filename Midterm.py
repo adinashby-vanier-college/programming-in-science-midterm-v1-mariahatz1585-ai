66b7dbc86ecac0b7d53d3bace11befc473ba3b31
mariahatz1585-ai <maria.hatz.1585@gmail.com>
2026-01-13 12:00:55 -0500

    Midterm M. Hatzipetros
    Implemented functions to calculate the area of a circle, generate a hollow right triangle, and create an inverted pyramid pattern. Each function includes input validation and formatted output.

--- Midterm.py
+++ Midterm.py
@@ -2,16 +2,50 @@ import math
 
 # Q1: Calculate the area of a circle
 def area_of_circle(radius):
-    return 0.0
+    r = float(radius)
+    area = math.pi * (r ** 2)
+    
+    result = round(area, 2)
+    return result 
 
 # Q2: Hollow Right Triangle
 def hollow_right_triangle(n):
-    return ""
+    result = ""
+
+    if n < 4:
+        result += "The triangle height should be at least 4."
+    
+    else:
+        for i in range(n):
+            
+            if i == n - 1 or i == 0:
+                result += ( i + 1) * "*" 
+            
+            else:
+                result += "*"
+                result += (i - 1) * " "
+                result += "*"
+
+            result += "\n"
+
+    return result.rstrip()
+
+
 
 # Q3: Inverted Pyramid
 def inverted_pyramid(n):
-    return ""
-
+    result = ""
+    
+    if n < 3:
+        result += "The pyramid height should be at least 3."
+   
+    else:
+        for i in range(n):
+            result += i * " "
+            result += (2 * n - 2 * i - 1) * "*"
+            result += "\n"
+    
+    return result.rstrip()
 # ----------------------------------------------------------------
 print(area_of_circle(5))
 print()
